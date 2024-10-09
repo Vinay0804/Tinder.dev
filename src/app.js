90735983ba99c92b040959f95be35e47904f3f58
Vinay Motapalukula <vinaychinna0804@gmail.com>
2024-10-09 06:54:07 +0530

    playing with multiple route handlers

--- src/app.js
+++ src/app.js
@@ -2,18 +2,29 @@ const server = require("express");
 const app = server();
 
 
-app.get("/hello",(req,res)=>{
-    res.send({firstname:"Vinay", LastName :"Motapalukula" })
+app.use("/hello",(req,res,next)=>{
+    console.log("1st response handled");
+
+    //res.send("1st response")
+    next();
+   
+},(req,res,next)=>{
+    console.log("2nd respone is handling")
+    //res.send("2nd response")
+    next();
+},(req,res,next)=>{
+    console.log("2nd respone is handling")
+    //res.send("2nd response")
+    next();
+},
+   (req,res,next)=>{
+    console.log("3nd respone is handling")
+    res.send("4nd response")
+    next()
 })
 
-app.post("/hello",(req,res)=>{
-    res.send("hello hello ")
-})
-app.delete("/hello",(req,res)=>{
-    res.send("successfully deleted ")
-})
-app.use("/",(req,res)=>{
-    res.send("welcome to the server");
-})
 
-app.listen(3000)
+
+app.listen(3000,()=>{
+    console.log("3000 port is listening")
+})
