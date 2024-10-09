c04e816865ce21bb868f786b91d8695e255e3777
Vinay Motapalukula <vinaychinna0804@gmail.com>
2024-10-09 22:29:32 +0530

    gettibg response from client side

--- src/app.js
+++ src/app.js
@@ -1,7 +1,33 @@
-const server = require("express");
+const express = require("express");
 const connectDB = require("./config/database");
-const app = server();
+const app = express();
+const User = require("./models/user")
 
+app.use(express.json());
+
+
+
+app.get("/user",async(req,res)=>{
+    const usermail = req.body.email;
+    try{
+     const user = await User.find({email:usermail});
+     res.send(user);
+    }catch{
+        res.status(400).send("something went wrong");
+    }
+})
+
+
+app.post("/signup", async(req,res)=>{
+const user = new User(req.body);
+
+try{ await user.save();
+    res.send("response send successfully")
+   }catch(err){
+    res.status(400).send("Error occured"); 
+   }
+
+})
 connectDB().then(()=>{
     console.log("Database connection established");
     app.listen(3000,()=>{
