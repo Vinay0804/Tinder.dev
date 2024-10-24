5d67f7b0ca1615101d943188137365ffeff3ea15
Vinay Motapalukula <vinaychinna0804@gmail.com>
2024-10-24 12:04:45 +0530

    Api level data sanitization

--- src/app.js
+++ src/app.js
@@ -37,10 +37,21 @@ app.delete("/user",async(req,res)=>{
        }
 })
 
-app.patch("/user",async(req,res)=>{
-    const userId = req.body.userId;
+app.patch("/user/:userId",async(req,res)=>{
+    const userId = req.params?.userId;
     const data = req.body;
+
     try{
+        const ALLOWED_UPDATES = ["firstName","lastName","age","gender","about","skills",];
+        const isUpdatesAllowed = Object.keys(data).every((k)=>
+           ALLOWED_UPDATES.includes(k)
+        );
+        if(!isUpdatesAllowed){
+            throw new Error("Upadates not Allowed")
+        }
+        if(data.skills.length>25){
+            throw new Error("Skills are not allowed")
+        }
         const user = await User.findByIdAndUpdate({ _id: userId},data,
             {
              runValidators: true,
@@ -50,7 +61,7 @@ app.patch("/user",async(req,res)=>{
         res.send("User updated successfully");
     }
     catch(err){
-        res.status(400).send("Something went wrong")
+        res.status(400).send("update failed: " + err.message)
     }
 
 });
