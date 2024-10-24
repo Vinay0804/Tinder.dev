63a1cb3aec909564bd5a33cb970c1825a943a97d
Vinay Motapalukula <vinaychinna0804@gmail.com>
2024-10-24 16:22:17 +0530

    exploree validator library

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose")
 
+const validator = require('validator');
+
 const userSchema = new mongoose.Schema({
     firstName : {
         type : String,
@@ -16,10 +18,20 @@ const userSchema = new mongoose.Schema({
         required : true,
         unique :true,
         trim : true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("email address is not valid :" +value);
+            }
+        },
     },
     password :{
         type :String,
-        required : true
+        required : true,
+        validate(value){
+            if(!validator.isStrongPassword(value)){
+                throw new Error("Enter a Strong password:" +value);
+            }
+        },
     },
     age :{
         type : Number,
