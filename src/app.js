d48d39b88a0a3e47bbe41194b2095500c2d47b40
Vinay Motapalukula <vinaychinna0804@gmail.com>
2024-10-24 11:25:28 +0530

    schema validations

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@ const User = require("./models/user")
 app.use(express.json());
 
 
+app.post("/signup", async(req,res)=>{
+    const user = new User(req.body);
+    
+    try{ await user.save();
+        res.send("response send successfully")
+       }catch(err){
+        res.status(400).send("Error occured"); 
+       }
+    
+    })
 
 app.get("/user",async(req,res)=>{
     const usermail = req.body.email;
@@ -17,17 +27,34 @@ app.get("/user",async(req,res)=>{
     }
 })
 
-
-app.post("/signup", async(req,res)=>{
-const user = new User(req.body);
-
-try{ await user.save();
-    res.send("response send successfully")
-   }catch(err){
-    res.status(400).send("Error occured"); 
-   }
-
+app.delete("/user",async(req,res)=>{
+    username = req.body.firstName
+    try{
+        await User.deleteMany({firstName:username})
+        res.send("succesfully deleted")
+    }catch(err){
+        res.status(400).send("Error occured"); 
+       }
 })
+
+app.patch("/user",async(req,res)=>{
+    const userId = req.body.userId;
+    const data = req.body;
+    try{
+        const user = await User.findByIdAndUpdate({ _id: userId},data,
+            {
+             runValidators: true,
+        }
+    );
+        console.log(user);
+        res.send("User updated successfully");
+    }
+    catch(err){
+        res.status(400).send("Something went wrong")
+    }
+
+});
+
 connectDB().then(()=>{
     console.log("Database connection established");
     app.listen(3000,()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,49 @@ const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
     firstName : {
-        type : String
+        type : String,
+        required : true,
+        minLength : 4,
+        maxLength : 50,
     },
     lastName :{
         type : String
     },
     email :{
-        type : String
+        type : String,
+        lowercase:true,
+        required : true,
+        unique :true,
+        trim : true,
     },
     password :{
-        type :String
+        type :String,
+        required : true
     },
     age :{
-        type : Number
+        type : Number,
+        min:18,
+        max:100
     },
-    grnder :{
-        type : String
-    }
+    gender :{
+        type : String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+                throw new Error("Gender data i not valid")
+            }
+        }
+    },
+    about :{
+        type : String,
+        default : "I was a developer working under xyz company"
+    },
+    skills :{
+        type :[String]
+    },
+
+    
+},{
+    timestamps:true,
 })
 
 module.exports = mongoose.model("User", userSchema);
