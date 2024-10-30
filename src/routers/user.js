563b07e3acd831976489f06377a51f6417d55278
Vinay Motapalukula <vinaychinna0804@gmail.com>
2024-10-30 19:32:14 +0530

    created all user connection and user requests api

--- src/routers/user.js
+++ src/routers/user.js
@@ -21,6 +21,33 @@ userRouter.get("/user/requests",Userauth, async (req,res)=>{
     
 })
 
+userRouter.get("/user/connections",Userauth,async(req,res)=>{
+    try{const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequest.find({
+        $or:[
+            
+            {fromUserId:loggedInUser._id ,status : "accepted"},
+            {toUserId:loggedInUser._id,status : "accepted"}
+        ]
+    }).populate("fromUserId", ["firstName","lastName","age","gender","skills","about"])
+    .populate("toUserId",["firstName","lastName","age","gender","skills","about"])
+
+
+     const data = connectionRequests.map((row)=>{
+        if(row.fromUserId._id.toString()===loggedInUser._id.toString()){
+            return row.toUserId
+        }
+        return row.toUserId
+     })
+
+     res.json({message:"connections requets", data})
+
+    }
+    catch(err){
+        res.status(400).send("ERROR :"+err.message)
+    }
+})
 
 
 module.exports = userRouter;
